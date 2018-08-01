6d2c2757873108ff1d9adb1b440589f0a7657034
Hugo Amnov <hugo.amiard@laposte.net>
2018-08-02 00:05:23 +0200

    Add Up/Down in Free Camera mode

--- jams/platform/ex_platform.cpp
+++ jams/platform/ex_platform.cpp
@@ -975,7 +975,7 @@ int main(int argc, char *argv[])
 	GameShell app(carray<cstring, 1>{ TOY_RESOURCE_PATH }, argc, argv);
 	
 	PlatformModule module = { _platform::m() };
-	app.run_game(module);
-	//app.run_editor(module);
+	//app.run_game(module);
+	app.run_editor(module);
 }
 #endif
