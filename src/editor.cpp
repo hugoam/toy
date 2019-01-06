ae35cd89d12ae5bc1eb69bafcc14583bdf74c195
Hugo Amnov <hugo.amiard@laposte.net>
2019-01-06 16:53:01 +0100

    Fix build

--- src/editor.cpp
+++ src/editor.cpp
@@ -1,6 +1,12 @@
 #include <toy/toy.h>
 #include <shell/Shell.h>
 
+#include <meta/core/Module.h>
+#include <meta/visu/Module.h>
+#include <meta/edit/Module.h>
+#include <meta/block/Module.h>
+#include <meta/shell/Module.h>
+
 using namespace mud; using namespace toy;
 
 int main(int argc, char *argv[])
@@ -12,6 +18,9 @@ int main(int argc, char *argv[])
 		std::string game = argv[1];
 		app.run_editor_path(game);
 	}
+
+	// @kludge for link error
+	toy_util::m(); toy_core::m(); toy_visu::m(); toy_edit::m(); toy_block::m();
 
 	//app.run_editor_path("ex_space");
 	//app.run_editor_path("ex_platform");
