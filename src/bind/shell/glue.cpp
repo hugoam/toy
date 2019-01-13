578fa498e9ce3080bfc4e2846849ff261d6b0133
Hugo Amnov <hugo.amiard@laposte.net>
2019-01-13 14:06:32 +0100

    Update JS glue

--- src/bind/shell/glue.cpp
+++ src/bind/shell/glue.cpp
@@ -32,14 +32,6 @@
 
 extern "C" {
 	
-	// Not using size_t for array indices as the values used by the javascript code are signed.
-	void array_bounds_check(const int array_size, const int array_idx) {
-		  if (array_idx < 0 || array_idx >= array_size) {
-			    EM_ASM({
-				      throw 'Array index ' + $0 + ' out of bounds: [0,' + $1 + ')';
-				    }, array_idx, array_size);
-			  }
-	}
 	// Game
 	toy::User* EMSCRIPTEN_KEEPALIVE Game_get_user(toy::Game* self) {
 		return self->m_user;
