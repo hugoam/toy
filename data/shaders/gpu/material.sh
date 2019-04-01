e9113de54754e0160aa23184abae332ab59947a4
Hugo Amnov <hugo.amiard@laposte.net>
2019-04-02 01:22:29 +0200

    Cleanup

--- data/shaders/gpu/material.sh
+++ data/shaders/gpu/material.sh
@@ -11,8 +11,8 @@
 #ifdef MATERIALS_BUFFER
 SAMPLER2D(s_materials, 8);
 #else
-uniform vec4 u_material_p0;
-uniform vec4 u_material_p1;
+uniform highp vec4 u_material_p0;
+uniform highp vec4 u_material_p1;
 #endif
 
 struct BaseMaterial
@@ -67,7 +67,7 @@ AlphaMaterial read_alpha_material(int index)
 }
 
 #ifndef MATERIALS_BUFFER
-uniform vec4 u_color;
+uniform highp vec4 u_color;
 #endif
 
 struct SolidMaterial
@@ -91,7 +91,7 @@ SolidMaterial read_solid_material(int index)
 }
 
 #ifndef MATERIALS_BUFFER
-uniform vec4 u_point_p0;
+uniform highp vec4 u_point_p0;
 #endif
 
 struct PointMaterial
@@ -119,7 +119,7 @@ PointMaterial read_point_material(int index)
 }
 
 #ifndef MATERIALS_BUFFER
-uniform vec4 u_line_p0;
+uniform highp vec4 u_line_p0;
 #endif
 
 struct LineMaterial
@@ -153,8 +153,8 @@ LineMaterial read_line_material(int index)
 }
 
 #ifndef MATERIALS_BUFFER
-uniform vec4 u_lit_p0;
-uniform vec4 u_emissive;
+uniform highp vec4 u_lit_p0;
+uniform highp vec4 u_emissive;
 #endif
 
 struct LitMaterial
@@ -190,11 +190,11 @@ LitMaterial read_lit_material(int index)
 }
 
 #ifndef MATERIALS_BUFFER
-uniform vec4 u_albedo;
-uniform vec4 u_pbr_p0;
-uniform vec4 u_pbr_channels_0;
-uniform vec4 u_pbr_p1;
-uniform vec4 u_pbr_p2;
+uniform highp vec4 u_albedo;
+uniform highp vec4 u_pbr_p0;
+uniform highp vec4 u_pbr_channels_0;
+uniform highp vec4 u_pbr_p1;
+uniform highp vec4 u_pbr_p2;
 #endif
 
 struct PbrMaterial
@@ -272,9 +272,9 @@ PbrMaterial read_pbr_material(int index)
 }
 
 #ifndef MATERIALS_BUFFER
-uniform vec4 u_diffuse;
-uniform vec4 u_specular;
-uniform vec4 u_phong_p0;
+uniform highp vec4 u_diffuse;
+uniform highp vec4 u_specular;
+uniform highp vec4 u_phong_p0;
 #endif
 
 struct PhongMaterial
--- data/shaders/line_fs.sc
+++ data/shaders/line_fs.sc
@@ -7,7 +7,6 @@
 $input v_color DASH_PARAMS
 
 #include <common.sh>
-#include <convert.sh>
 
 void main()
 {
@@ -20,7 +19,6 @@ void main()
 
 #ifdef DASH
     if (mod(v_line_distance, mat.dash_size + mat.dash_gap) > mat.dash_size) discard; // todo - FIX
-    v_color = vec4(hsl_to_rgb(vec3(v_line_distance, 1.0, 0.5)), 1.0);
 #endif
 
     vec4 color = vec4(solid.color.rgb, solid.color.a * alpha);
